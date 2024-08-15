1a7d709884954a4e9fe4605b4d07e68a8d0e685a
Vikas Sahni <vikassahni8361@gmail.com>
2024-08-16 00:03:59 +0530

    update in the UI

---
+++ script.js
@@ -0,0 +1,8 @@
+const floorInput = document.querySelector(".floor-input-container input");
+const LiftInput = document.querySelector(".lift-input-container input");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  const floors = floorInput.value;
+  const lifts = LiftInput.value;
+});
